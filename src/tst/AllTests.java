08e2687a54ef3ce8006d66a5a10bf8b1bfcc085e
Leonardo Gonçalves Machado <leonardogm2003@gmail.com>
2024-12-13 17:17:16 -0300

    parametrrizando quase tudo e atualizando all tests

--- src/tst/AllTests.java
+++ src/tst/AllTests.java
@@ -10,7 +10,9 @@ import org.junit.runners.Suite.SuiteClasses;
 				TesteCalculosDeducoesDependentes.class, 
 				TesteCadastroContribuicaoPrevidenciaria.class, 
 				TesteCadastroPensaoAlimenticia.class, 
-				TesteCadastroOutrasDeducoes.class})
+				TesteCadastroOutrasDeducoes.class,
+				TesteAliquotaEfetiva.class,
+				TesteBaseCalculo.class})
 public class AllTests {
 
 }
